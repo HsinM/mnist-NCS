7aff6e590d835410ea962d2233590ac5ab2a903f
Hsin <hsin40911@gmail.com>
2018-05-20 16:32:55 +0800

    something added

--- train-minst.py
+++ train-minst.py
@@ -4,8 +4,7 @@
 # In[1]:
 
 
-from keras import layers
-from keras import models
+from keras import layers models
 from keras.models import load_model
 from keras.datasets import mnist
 from keras.utils import to_categorical
@@ -39,50 +38,50 @@ y_test = to_categorical(y_test)
 # In[5]:
 
 
-cnn = models.Sequential()
-cnn.add(layers.Conv2D(16, 3, activation='relu', input_shape=(28, 28, 1)))
-cnn.add(layers.MaxPool2D())
-cnn.add(layers.Conv2D(32, 3, activation='relu'))
-cnn.add(layers.MaxPool2D())
-cnn.add(layers.Conv2D(64, 3, activation='relu'))
-cnn.add(layers.MaxPool2D())
-cnn.add(layers.Flatten())
-cnn.add(layers.Dense(256, activation='relu'))
-cnn.add(layers.Dropout(0.5))
-cnn.add(layers.Dense(10, activation='softmax'))
-cnn.summary()
+model = models.Sequential()
+model.add(layers.Conv2D(16, 3, activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPool2D())
+model.add(layers.Conv2D(32, 3, activation='relu'))
+model.add(layers.MaxPool2D())
+model.add(layers.Conv2D(64, 3, activation='relu'))
+model.add(layers.MaxPool2D())
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
 
 
 # In[6]:
 
 
-cnn.compile(optimizer='adam', metrics=['accuracy'], loss='categorical_crossentropy')
+model.compile(optimizer='adam', metrics=['accuracy'], loss='categorical_crossentropy')
 
 
-# In[ ]:
+# In[7]:
 
 
-history = cnn.fit(x_train, y_train, epochs=20, batch_size=128)
+history = model.fit(x_train, y_train, epochs=20, batch_size=128)
 
 
-# In[ ]:
+# In[8]:
 
 
-print(cnn.evaluate(x_test, y_test))
+print(model.evaluate(x_test, y_test))
 
 
-# In[ ]:
+# In[9]:
 
 
 # model 與 weights 分別儲存
 with open("model.json", "w") as file:
-    file.write(cnn.to_json())
-cnn.save_weights("weights.h5")
+    file.write(model.to_json())
+model.save_weights("weights.h5")
 
 
-# In[ ]:
+# In[10]:
 
 
-# model 與 weights存在同意檔案中，for convert-mnist-only-h5.ipynb
-cnn.save('model.h5')
+# model 與 weights存在同一檔案中,for convert-mnist-only-h5.ipynb
+model.save('model.h5')
 
